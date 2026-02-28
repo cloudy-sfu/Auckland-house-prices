b67aa962910f1b6eb76b85b5aabd0ba97ad6a598
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2026-03-01 10:33:20 +1300

    Fix https://github.com/cloudy-sfu/Auckland-house-prices/actions/runs/22522832865/job/65250225071

---
+++ internet_outage/delete_failed_tlc_trademe.sql
@@ -0,0 +1,7 @@
+delete from properties_trademe_chorus_tlc
+where tlc is not null
+and not exists (
+    select 1
+    from internet_availability
+    where properties_trademe_chorus_tlc.tlc = internet_availability.tlc
+);
