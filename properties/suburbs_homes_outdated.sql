6edd67c6115389fc4484058854dc828d14856ecc
cloudy-sfu <41314224+cloudy-sfu@users.noreply.github.com>
2026-03-17 01:01:50 +1300

    In homes.co.nz crawler, exclude suburbs of no house

--- properties/suburbs_homes_outdated.sql
+++ properties/suburbs_homes_outdated.sql
@@ -5,6 +5,15 @@ with suburbs_last_updated_time as (
         max(search_time_utc) as last_updated_time
     from suburbs left join properties_homes
         on suburbs.suburb_id = properties_homes.suburb_id
+    where suburbs.suburb_id not in (
+        -- Suburbs of no house
+        113700 -- Riverhead Forest
+        ,154600 -- Botany Central
+        ,161700 -- Takanini Industrial
+        ,149600 -- Ōtāhuhu Industrial
+        ,138800 -- Wesley South
+        ,147700 -- Mount Wellington Industrial
+        )
     group by suburbs.suburb_id
 )
 select suburb_id, geometry -> 'coordinates' as coordinates
